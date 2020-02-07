6d27291a4964b228769eda62f24998e1641ea9e8
rovm <rovm92@naver.com>
2020-02-07 14:30:19 +0900

    vue
    List 자동 갱신

--- vueCLI/src/components/TodoInput.vue
+++ vueCLI/src/components/TodoInput.vue
@@ -24,6 +24,7 @@ export default {
       if(this.newTodoItem !== ""){
         var value = this.newTodoItem && this.newTodoItem.trim();
         localStorage.setItem(value, value);
+        this.$emit('addTodo', value);
         this.clearInput();
       }
     },
