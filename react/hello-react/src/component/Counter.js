a7821eae6007c8e9e0d4db7f8594fa65362b0d88
rovm <rovm92@naver.com>
2021-03-14 21:02:06 +0900

    .

--- react/hello-react/src/component/Counter.js
+++ react/hello-react/src/component/Counter.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react';
 
 class Counter extends Component{
+    /*
     constructor(props){
         super(props);
         // state의 초기값 설정하기
         this.state = {
-            number : 0
+            number : 0,
+            fixedNumber: 0
         }
+    }*/
+    state = {
+        number : 0,
+        fixedNumber: 0
     }
 
     render(){
-        const {number} = this.state;
+        const {number, fixedNumber} = this.state; //state를 조회할 때는 .this.state로 조회합니다.
 
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button
                     //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
                     onClick = {() =>{
-                         this.setState({number: number + 1});
+                         //this.setState({number: number + 1});
+                         //this.setState({number: this.state.number + 1});
+                         this.setState(prevState =>{
+                             return {
+                                 number: prevState.number + 1
+                             };
+                         });
+
+                         this.setState(prevState => ({
+                            number: prevState.number +1
+                         }));
                         }
                     }
                 >
