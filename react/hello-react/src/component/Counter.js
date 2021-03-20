0325de2a127b74d7615fff6b9e3f3853bbf3db04
rovm <rovm92@naver.com>
2021-03-20 22:42:54 +0900

    .

--- react/hello-react/src/component/Counter.js
+++ react/hello-react/src/component/Counter.js
@@ -27,15 +27,23 @@ class Counter extends Component{
                     onClick = {() =>{
                          //this.setState({number: number + 1});
                          //this.setState({number: this.state.number + 1});
-                         this.setState(prevState =>{
-                             return {
-                                 number: prevState.number + 1
-                             };
-                         });
+                        //  this.setState(prevState =>{
+                        //      return {
+                        //          number: prevState.number + 1
+                        //      };
+                        //  });
 
-                         this.setState(prevState => ({
-                            number: prevState.number +1
-                         }));
+                        //  this.setState(prevState => ({
+                        //     number: prevState.number +1
+                        //  }));
+                            this.setState({
+                                number : number + 1
+                            },
+                            () => {
+                                    console.log('방금 setState가 호출되었습니다.');
+                                    console.log(this.state)
+                                }
+                            );
                         }
                     }
                 >
