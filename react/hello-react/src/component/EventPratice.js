582887448454a05e9db7b74845ac77984684d0af
rovm <rovm92@naver.com>
2021-03-21 20:12:48 +0900

    function component
    function component

--- react/hello-react/src/component/EventPratice.js
+++ react/hello-react/src/component/EventPratice.js
@@ -1,68 +1,66 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class EventPratice extends Component{
-    state ={
+const EventPratice = () => {
+    // const [username, setUsername] = useState('');
+    // const [message, setMessage] = useState('');
+    // const onChangeUsername = e => setUsername(e.target.value);
+    // const onChangeMessage = e => setMessage(e.target.value);
+
+    const [form, setForm] = useState({
         username: '',
-        message : ''
-    }
+        message: ''
+    });
 
-    constructor(props){
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
+    const {username, message} = form;
 
-    handleChange = (e) => {
-        this.setState({
-            //message:e.target.value
-            [e.target.name]:e.target.value
+    const onChange = e => {
+        const nextForm = {
+            ...form,
+            [e.target.name]: e.target.value
+        };
+        setForm(nextForm);
+    };
+
+    const onClick = () =>{
+        alert(username + ': ' + message);
+        //setUsername('');
+        //setMessage('');
+        setForm({
+            username: '',
+            message: ''
         });
-    }
+    };
 
-    handleClick = () => {
-        // alert(this.state.message);
-        // this.serState({
-        //     message:''
-        // });
-        alert(this.state.username + ': ' + this.state.message)
-        this.setState({
-            username:'',
-            message:''
-        });
-    }
+    const onKeyPress = e => {
+        if(e.key === 'Enter'){
+            onClick();
+        };
+    };
 
-    handleKeyPress = (e) =>{
-        if(e.key == 'Enter'){
-            this.handleClick();
-        }
-    }
+    return (
+        <div>
+            <h1>이벤트 연습</h1>
 
-    render(){
-        return(
-            <div>
-                <h1>이벤트 연습</h1>
+            <input
+                type='text'
+                name='username'
+                placeholder='사용자명'
+                value={username}
+                onChange={onChange}
+            />
 
-                <input
-                    type='text'
-                    name='username'
-                    placeholder='사용자명'
-                    value={this.state.username}
-                    onChange={this.handleChange}
-                />
+            <input
+                type='text'
+                name='message'
+                placeholder='아무거나 입력해 보세요'
+                value={message}
+                onChange={onChange}
+                onKeyPress={onKeyPress}
+            />
 
-                <input
-                    type='text'
-                    name='message'
-                    placeholder='아무거나 입력해 보세요'
-                    value={this.state.message}
-                    onChange={this.handleChange}
-                    onKeyPress={this.handleKeyPress}
-                />
-
-                <button onClick={this.handleClick}>확인</button>
-            </div>
-        ); 
-    }
-}
+            <button onClick={onClick}>확인</button>
+        </div>
+    ); 
+};
 
 export default EventPratice;
