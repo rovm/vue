09abb8534058c18d496dcf9fe981dd08d644dcbb
rovm <rovm92@naver.com>
2021-03-23 22:43:37 +0900

    useEffect

--- react/hooks-tutorial/src/component/Info.js
+++ react/hooks-tutorial/src/component/Info.js
@@ -4,13 +4,25 @@ const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
 
-    useEffect(() => {
-        console.log('렌더링이 완료되었습니다.');
-        console.log({
-            name,
-            nickname
-        })
-    }, []);
+    // useEffect(() => {
+    //     console.log('렌더링이 완료되었습니다.');
+    //     console.log({
+    //         name,
+    //         nickname
+    //     })
+    // }, [name]); 
+    /* 두번쨰 인자에 빈 []를 넣어주면, 마운트 될때만 실행.
+    *  []안에 넣은 파라미터가 바뀔떄마다 실행
+    */
+
+    useEffect(() =>{
+        console.log('effect');
+        console.log(name);
+        return() => {
+            console.log('cleanup');
+            console.log(name);
+        };
+    }, [name]);
 
     const onChangeName = e => {
         setName(e.target.value);
