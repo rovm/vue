e8e18d762c326ac861f6402a3b40149cc9fd39b5
rovm <rovm92@naver.com>
2021-03-23 22:28:00 +0900

    multiple useState

--- react/hooks-tutorial/src/App.js
+++ react/hooks-tutorial/src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import Counter from'./component/Counter';
+import React from 'react';
+import Info from'./component/Info';
 
 const App = () => {
-  return <Counter/>;
+  return <Info/>;
 }
 
 export default App;
