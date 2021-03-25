fbb976ab7764fa626f96832bee9c3a70b7917445
rovm <rovm92@naver.com>
2021-03-25 22:06:01 +0900

    useReducer

--- react/hooks-tutorial/src/App.js
+++ react/hooks-tutorial/src/App.js
@@ -1,23 +1,8 @@
 import React, { useState } from 'react';
-import Info from'./component/Info';
+import Counter from'./component/Counter';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-
-  return(
-    <div>
-      <button
-        onClick={()=> {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr/>
-      {visible && <Info/>}
-    </div>
-  )
-
+  return <Counter/>
 }
 
 export default App;
---
+++ react/hooks-tutorial/src/App_01.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Info from'./component/Info';
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+
+  return(
+    <div>
+      <button
+        onClick={()=> {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr/>
+      {visible && <Info/>}
+    </div>
+  )
+
+}
+
+export default App;
