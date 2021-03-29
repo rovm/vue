7952c51f94ad12d39b1d6421b8ca29aca25666af
rovm <rovm92@naver.com>
2021-03-29 22:48:32 +0900

    add TodoListItem, TodoList
    create TodoListItem, TodoList

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Todotemplate from './component/TodoTemplate';
 import TodoInsert from './component/TodoInsert';
+import TodoList from './component/TodoList';
 
 const App = () => {
   return (
     <Todotemplate>
       <TodoInsert/>
+      <TodoList/>
     </Todotemplate>
   );
 }
---
+++ react/todo-app/src/component/TodoListItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    MdCheckBoxOutlineBlank,
+    
+    MdRemoveCircleOutline 
+} from 'react-icons/md'
+import './TodoListItem.scss';
+
+const TodoList = () => {
+    return(
+        <div className="TodoListItem">
+            <div className="checkBox">
+                <MdCheckBoxOutlineBlank/>
+                <div className="text">할 일</div>
+            </div>
+            <div className="remove">
+                <MdRemoveCircleOutline />
+            </div>
+        </div>
+    )
+}
+
+export default TodoList;
