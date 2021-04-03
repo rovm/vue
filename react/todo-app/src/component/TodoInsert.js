c01cf182a68e4c9296f944b4928c73c6349559bd
rovm <rovm92@naver.com>
2021-04-03 23:45:24 +0900

    todo insert value state

--- react/todo-app/src/component/TodoInsert.js
+++ react/todo-app/src/component/TodoInsert.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 /* https://react-icons.netlify.com/#/icons/md */
 import {MdAdd} from 'react-icons/md';
 import './TodoInsert.scss';
 
 const TodoInsert = () => {
+    const [value, setValue] = useState('');
+    
+    const onChange = useCallback(e => {
+        setValue(e.target.value)
+    }, [])
     return(
         <form className="TodoInsert">
-            <input placeholder="할일을 입력하세요."></input>
+            <input placeholder = "할일을 입력하세요."
+                   value = {value}
+                   onChange = {onChange}
+            ></input>
             <button type="submit">
                 <MdAdd/>
             </button>
