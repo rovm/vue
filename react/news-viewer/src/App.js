aa05ff24f17fdeefc16594f3d56f94f4481ebaaa
rovm <rovm92@naver.com>
2021-04-11 20:03:03 +0900

    add newsItem, newsList

--- react/news-viewer/src/App.js
+++ react/news-viewer/src/App.js
@@ -1,25 +1,27 @@
 import React, {useState} from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response =  await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=3f6c724effb94cc3b1b76014785f2d62',);
-      setData(response.data)  
-    } catch (e){
-      console.log(e)
-    }
-  };
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response =  await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=3f6c724effb94cc3b1b76014785f2d62',);
+  //     setData(response.data)  
+  //   } catch (e){
+  //     console.log(e)
+  //   }
+  // };
 
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 1)} readOnly={true}/>}
-    </div>
-  )
+  // return (
+  //   <div>
+  //     <div>
+  //       <button onClick={onClick}>불러오기</button>
+  //     </div>
+  //     {data && <textarea rows={7} value={JSON.stringify(data, null, 1)} readOnly={true}/>}
+  //   </div>
+  // )
+  return <NewsList/>
 };
 
 export default App;
