cd2291f0513fab7f21e650908e21c19e46e9cf6c
rovm <rovm92@naver.com>
2021-04-12 22:47:16 +0900

    category ui

--- react/news-viewer/src/App.js
+++ react/news-viewer/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 // import axios from 'axios';
 import NewsList from './components/NewsList';
 import Categories from './components/Categories';
@@ -22,10 +22,13 @@ const App = () => {
   //     {data && <textarea rows={7} value={JSON.stringify(data, null, 1)} readOnly={true}/>}
   //   </div>
   // )
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category),[]);
+
   return (
     <>
-      <Categories/>
-      <NewsList/>
+      <Categories category={category} onSelect={onSelect}/>
+      <NewsList category={category}/>
     </>
   )
 };
