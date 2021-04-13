206d67f12047b55e494c14a04b29b5e70212722b
rovm <rovm92@naver.com>
2021-04-13 23:03:54 +0900

    newsPage

---
+++ react/news-viewer/src/AppVer1.js
@@ -0,0 +1,36 @@
+import React, { useCallback, useState } from 'react';
+// import axios from 'axios';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+
+const App = () => {
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response =  await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=3f6c724effb94cc3b1b76014785f2d62',);
+  //     setData(response.data)  
+  //   } catch (e){
+  //     console.log(e)
+  //   }
+  // };
+
+  // return (
+  //   <div>
+  //     <div>
+  //       <button onClick={onClick}>불러오기</button>
+  //     </div>
+  //     {data && <textarea rows={7} value={JSON.stringify(data, null, 1)} readOnly={true}/>}
+  //   </div>
+  // )
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category),[]);
+
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect}/>
+      <NewsList category={category}/>
+    </>
+  )
+};
+
+export default App;
