948761a9f1cb1499e2f261acc27ae15a92317530
rovm <rovm92@naver.com>
2021-04-25 19:38:29 +0900

    useActions util hooks

--- react/react-redux-tutorial/src/containers/TodosContainer.js
+++ react/react-redux-tutorial/src/containers/TodosContainer.js
@@ -3,6 +3,7 @@ import React, { useCallback } from "react";
 import {useSelector, useDispatch} from 'react-redux';
 import todos, {changeInput, insert, toggle, remove} from '../modules/todos';
 import Todos from '../components/Todos';
+import useActions from "../lib/useActions";
 
 // const TodosContainer = ({
 //     input,
@@ -43,13 +44,17 @@ const TodosContainer = () => {
         todos: todos.todos
     }));
 
-    const dispatch = useDispatch();
-    const onChangeInput = useCallback(input => dispatch(changeInput(input)), [
-        dispatch
-    ]);
-    const onInsert = useCallback(text => dispatch(insert(text)),[dispatch]);
-    const onToggle = useCallback(id => dispatch(toggle(id)),[dispatch]);
-    const onRemove = useCallback(id => dispatch(remove(id)),[dispatch]);
+    // const dispatch = useDispatch();
+    // const onChangeInput = useCallback(input => dispatch(changeInput(input)), [
+    //     dispatch
+    // ]);
+    // const onInsert = useCallback(text => dispatch(insert(text)),[dispatch]);
+    // const onToggle = useCallback(id => dispatch(toggle(id)),[dispatch]);
+    // const onRemove = useCallback(id => dispatch(remove(id)),[dispatch]);
+    const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+        [changeInput, insert, toggle, remove],
+        []
+    )
 
     return (
         <Todos
