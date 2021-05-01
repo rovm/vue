442758a32b5a534d1323e70301db5b73bbb91b96
rovm <rovm92@naver.com>
2021-05-01 18:22:05 +0900

    redux logger

--- react/learn-redux-middleware/src/index.js
+++ react/learn-redux-middleware/src/index.js
@@ -7,8 +7,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './modules';
 import loggerMiddleware from './lib/loggerMiddleware';
+import {createLogger} from 'redux-logger';
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
