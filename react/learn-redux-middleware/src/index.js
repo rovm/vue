4f412170512cdf137968250b2f81440086143097
rovm <rovm92@naver.com>
2021-05-03 22:14:36 +0900

    composeWithDevTools

--- react/learn-redux-middleware/src/index.js
+++ react/learn-redux-middleware/src/index.js
@@ -10,10 +10,11 @@ import rootReducer, {rootSaga} from './modules';
 import {createLogger} from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk, sagaMiddleware));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware)));
 
 sagaMiddleware.run(rootSaga);
 
