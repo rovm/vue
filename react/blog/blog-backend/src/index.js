f7246a68a075dde21db766523d11fb325756aa35
rovm <rovm92@naver.com>
2021-05-17 23:27:50 +0900

    yarn add koa-bodyparser

--- react/blog/blog-backend/src/index.js
+++ react/blog/blog-backend/src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
 
@@ -47,6 +48,9 @@ router.use('/api', api.routes()); //api 라우트 적용
 //     //id의 존재 유무에 따라 다른 결과 출력
 //     ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
 // });
+
+//라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
