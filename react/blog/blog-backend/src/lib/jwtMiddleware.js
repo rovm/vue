6f243ac227c53974c506de8f32b8361d0296832b
rovm <rovm92@naver.com>
2021-05-30 22:47:28 +0900

    Update jwtMiddleware.js

--- react/blog/blog-backend/src/lib/jwtMiddleware.js
+++ react/blog/blog-backend/src/lib/jwtMiddleware.js
@@ -1,4 +1,5 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
+import User from '../models/user';
 
 const jwtMiddleware = (ctx, next) => {
     const token = ctx.cookies.get('access_token');
@@ -9,7 +10,16 @@ const jwtMiddleware = (ctx, next) => {
             _id: decoded._id,
             username: decoded.username,
         };
-        console.log(decoded);
+        //토큰의 남은 유효 기간이 3.5일 미만이면 재발급
+        const now = Math.floor(Date.now() / 1000);
+        if(decoded.exp - now < 60 *60 *24 *3.5){
+            const user = await User.findById(decoded._id);
+            const token = user.generateToken();
+            ctx.cookies.set('access_token', token, {
+                maxAge: 1000 * 60 * 60 *24 * 7, //7일
+                httpOnly: true,
+            });
+        }
         return next();
     } catch(e){
         return next();
