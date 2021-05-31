2587b6874e8c9606b6d082a87b3319cb3fbaa403
rovm <rovm92@naver.com>
2021-05-31 22:48:38 +0900

    .

--- react/blog/blog-backend/src/models/post.js
+++ react/blog/blog-backend/src/models/post.js
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose';
+import mongoose, {Schema} from 'mongoose';
 
-const {Schema} = mongoose;
+// const {Schema} = mongoose;
 
 const PostSchema = new Schema({
     title: String,
@@ -9,6 +9,10 @@ const PostSchema = new Schema({
     publishedDate: {
         type: Date,
         default: Date.now, //현재 날짜를 기본값으로 지정
+    },
+    user: {
+        _id: mongoose.Types.ObjectId,
+        username: String,
     },
 });
 
