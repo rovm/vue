589df75a588781c5857349115a5148897cfd97df
rovm <rovm92@naver.com>
2021-06-20 20:56:35 +0900

    setting snippet

---
+++ react/blog/blog-fontend/src/components/base/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderBlock = styled.div``;
+
+const Header = () => {
+    returb (
+        <HeaderBlock>
+            
+        </HeaderBlock>
+    );
+};
+
+export default Header;
