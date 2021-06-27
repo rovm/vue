10b9960b34becdb2cfafe8e54d594ce9d15b6d5f
rovm <rovm92@naver.com>
2021-06-27 23:07:08 +0900

    add api 요청관리

---
+++ react/blog/blog-fontend/src/modules/loading.js
@@ -0,0 +1,38 @@
+import { load } from 'js-yaml';
+import { createAction, handleActions} from 'redux-actions';
+
+const START_LOADING = 'loading/START_LOADING';
+const FINISH_LOADING = 'loading/FINISH_LOADING';
+
+/*
+ * 요청을 위한 액션 타입을 payload로 설정 
+ */
+
+export const startLoading = createAction(
+    START_LOADING,
+    requestType => requestType,
+);
+
+export const finishLoading = createAction(
+    FINISH_LOADING,
+    resultType => resultType,
+);
+
+const initialState = {};
+
+const loading = handleActions(
+    {
+        [START_LOADING]: (state, action) => ({
+            ...state,
+            [action.payload]: true,
+        }),
+        [FINISH_LOADING]: (state, action) => ({
+            ...state,
+            [action.payload]: false,
+        }),
+    },
+    initialState,
+);
+
+export default loading;
+
