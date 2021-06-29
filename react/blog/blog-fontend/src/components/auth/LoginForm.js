39e2b65d7b7d16aa605cc082eafaaab9aef4fd6e
rovm <rovm92@naver.com>
2021-06-29 23:48:13 +0900

    회원인증 에러처리하기

--- react/blog/blog-fontend/src/components/auth/LoginForm.js
+++ react/blog/blog-fontend/src/components/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
@@ -6,6 +6,8 @@ import { check } from '../../modules/user';
 import { withRouter } from 'react-router-dom';
 
 const LoginForm = ({history}) => {
+    const [error, setError] = useState(null);
+
     const dispatch = useDispatch();
     const {form, auth, authError, user} = useSelector(({auth, user}) => ({
         form: auth.login,
@@ -42,6 +44,7 @@ const LoginForm = ({history}) => {
         if(authError){
             console.log('오류 발생');
             console.log(authError);
+            setError('로그인 실패');
             return;
         }
         if(auth) {
@@ -63,6 +66,7 @@ const LoginForm = ({history}) => {
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     );
 };
