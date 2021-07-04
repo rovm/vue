fb16fe60c6ee202f4ffeaaa5b9bcb0cf0e600591
rovm <rovm92@naver.com>
2021-07-04 16:17:27 +0900

    로그인 상태 유지하기

--- react/blog/blog-fontend/src/components/auth/RegisterForm.js
+++ react/blog/blog-fontend/src/components/auth/RegisterForm.js
@@ -74,6 +74,11 @@ const RegisterForm = ({history}) => {
     useEffect(() => {
         if(user){
             history.push('/'); // 홈화면 이동
+            try{
+                localStorage.setItem('user', JSON.stringify(user));
+            } catch (e) {
+                console.log('localStorage is not working');
+            }
         }
     }, [history, user]);
 
--- react/blog/blog-fontend/src/index.js
+++ react/blog/blog-fontend/src/index.js
@@ -9,6 +9,7 @@ import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, {rootSaga} from './modules';
+import { tempSetUser, check } from './modules/user';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -16,7 +17,22 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
+function loadUser(){
+  console.log("index")
+  try {
+    const user = localStorage.getitem('user');
+    console.log(user)
+    if(!user) return; // 로그인 상태가 아니라면 아무것도 안 함
+
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+  } catch(e) {
+    console.log('localStorage is not working');
+  }
+}
+
 sagaMiddleware.run(rootSaga);
+loadUser();
 
 ReactDOM.render(
   <Provider store={store}>
