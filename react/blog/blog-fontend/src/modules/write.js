54029c63603f5f89126121d995c037750073b5e8
rovm <rovm92@naver.com>
2021-07-10 23:02:11 +0900

    리덕스로 글쓰기 상태 관리하기

---
+++ react/blog/blog-fontend/src/modules/write.js
@@ -0,0 +1,29 @@
+import { createAction, handleActions } from "redux-actions";
+
+const INITIALIZE = 'write/INITIALIZE'; // 모든 내용 초기화
+const CHANGE_FILED = 'write/CHANGE_FILED'; // 특정 key 값 바꾸기
+
+export const initialize = createAction(INITIALIZE);
+export const change_filed = createAction(CHANGE_FILED, ({key, value}) => ({
+    key,
+    value,
+}));
+
+const initialize = {
+    title: '',
+    body: '',
+    tags: [],
+};
+
+const write = handleActions(
+    {
+        [INITIALIZE]: state => initialState, // initialState를 넣으면 초기 상태로 바귐
+        [CHANGE_FILED]: (state, {payload: {key, value}}) => ({
+            ...state,
+            [key]: value, //특정 key 값을 업데이트 
+        }),
+    },
+    initialState,
+);
+
+export default write;
